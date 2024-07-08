ab3b8ebb06c0386fb94e6741f12e2e1a1cac21c9
L F <franco100lucas@gmail.com>
2024-07-07 23:10:04 -0300

    feat: Update routes.rb to set root path to "produtos/index"

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get "produtos", to: "produtos#index"
+  root to: "produtos#index"
+  # get "produtos", to: "produtos#index"
   # Requisição GET => para a rota produtos => para o controller produtos => para o método/view index
 end
