965db72bff3df69fe11222d61a5ea0754788de86
L F <franco100lucas@gmail.com>
2024-07-09 09:13:11 -0300

    feat: Add create method to ProdutosController for creating new products

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -3,4 +3,18 @@ class ProdutosController < ApplicationController
     @produtos = Produto.all.order :nome
     @produto_com_desconto = Produto.order(:preco).limit 2
   end
+
+  def create
+    produto_params = params.require(:produto).permit(:nome, :descricao, :preco, :quantidade)
+    # Estabelece os parâmetros que o método create irá receber
+    Produto.create produto_params
+    # Cria um novo produto com os parâmetros recebidos
+    redirect_to root_path
+    # Redireciona para a página raiz
+
+    # Na ordem, o método create faz:
+    # 1. Recebe os parâmetros do formulário
+    # 2. Cria um novo produto com esses parâmetros
+    # 3. Redireciona para a página raiz
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root to: "produtos#index"
-  # get "produtos", to: "produtos#index"
+  root to: "produtos#index" # Rota raiz, renderiza a página index do controller produtos
+  get "produtos/new", to:"produtos#new" # Buscar a página de criação de um novo produto
   # Requisição GET => para a rota produtos => para o controller produtos => para o método/view index
+
+  post "produtos", to:"produtos#create" # Rota para criar um novo produto
+  # Requisição POST => para a rota produtos => para o controller produtos => para o método create
 end
