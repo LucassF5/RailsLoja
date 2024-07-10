a3916262f2e827714bdf79afa3ffb3545433fb9e
L F <franco100lucas@gmail.com>
2024-07-09 22:50:33 -0300

    refactor: Update routes.rb to use Rails resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   root to: "produtos#index" # Rota raiz, renderiza a página index do controller produtos
-  get "produtos/new", to:"produtos#new" # Buscar a página de criação de um novo produto
+  # get "produtos/new", to:"produtos#new" # Buscar a página de criação de um novo produto
   # Requisição GET => para a rota produtos => para o controller produtos => para o método/view index
 
-  post "produtos", to:"produtos#create" # Rota para criar um novo produto
+  # post "produtos", to:"produtos#create" # Rota para criar um novo produto
   # Requisição POST => para a rota produtos => para o controller produtos => para o método create
 
-  delete "produtos/:id", to:"produtos#destroy", as: :produto
+  # delete "produtos/:id", to:"produtos#destroy", as: :produto
   # Requisição DELETE => para a rota produtos/:id => para o controller produtos => para o método destroy
+
+  resources :produtos, only: [:new, :create, :destroy]
 end
